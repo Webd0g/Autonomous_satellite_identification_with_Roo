2c51fd2097791736adfd515622d46140efa093fd
Webd0g <rileywebster35@gmail.com>
2025-07-31 20:09:57 +1000

    Delete Python_script_test_environment directory

--- Python_script_test_environment/Skyfield_tle_parser_test.py
+++
@@ -1,6 +0,0 @@
-from skyfield.api import load
-max_days = 3
-name = 'satellite_data.txt'
-base = 'https://celestrak.org/NORAD/elements/gp.php'
-url = base + '?GROUP=active&FORMAT=tle'
-if not load.exists(name) or load.days_old(name)
